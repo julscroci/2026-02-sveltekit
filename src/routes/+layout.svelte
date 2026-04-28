154e8d30226280410bcea07345c017a122b4384b
Giulia Croci <giulia1.croci@mail.polimi.it>
2026-04-28 23:18:19 +0200

    Add custom fonts

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,6 +8,16 @@
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+
+	<!-- Satoshi font -->
+	<link rel="preconnect" href="https://api.fontshare.com" />
+	<link rel="preconnect" href="https://api.fontshare.com" crossorigin />
+	<link href="https://api.fontshare.com/v2/css?f[]=satoshi@1,2&display=swap" rel="stylesheet">
+
+	<!-- Inter font -->
+	<link rel="preconnect" href="https://rsms.me/">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link rel="stylesheet" href="https://rsms.me/inter/inter.css">
 </svelte:head>
 
 {@render children()}
