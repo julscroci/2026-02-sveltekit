235e44cff26d589cb94d365f67e615b91c5ad935
Giulia Croci <giulia1.croci@mail.polimi.it>
2026-04-29 09:52:01 +0200

    Add header component

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,28 @@
+<script>
+    import Link from "$lib/components/Link.svelte";
+</script>
+
+<header class="safe-area">
+    <Link title="W-D" ref="/" />
+
+    <nav>
+        <Link title="About" ref="/about" />
+        <Link title="@julscroci" ref="https://www.instagram.com/julscroci" 
+        />
+    </nav>
+</header>
+
+
+<style>
+    header{
+        padding-block: var(--size-0);
+        display: flex; gap: var(--size-8); 
+        justify-content: space-between; 
+        align-items: center;
+    }
+    nav {
+        display: flex; 
+        gap: var(--size-6); 
+        align-items: center;
+    }
+</style>
