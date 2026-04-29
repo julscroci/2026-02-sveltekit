29d7c09bc0369cd5c99a73df5e18720ca7839923
Giulia Croci <giulia1.croci@mail.polimi.it>
2026-04-29 09:35:16 +0200

    Layout svelte completed

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,13 @@
 <script>
-	import favicon from '$lib/assets/favicon.svg';
+	import favicon from "$lib/assets/favicon.svg";
 
 	import "$lib/styles/reset.css";
 	import "$lib/styles/tokens.css";
+	import "$lib/styles/base.css";
+	import "$lib/styles/utilities.css";
+
+	import Header from "$lib/components/Header.svelte"; 
+	import Footer from "$lib/components/Footer.svelte";
 
 	let { children } = $props();
 </script>
@@ -10,15 +15,17 @@
 <svelte:head>
 	<link rel="icon" href={favicon} />
 
-	<!-- Satoshi font -->
-	<link rel="preconnect" href="https://api.fontshare.com" />
-	<link rel="preconnect" href="https://api.fontshare.com" crossorigin />
-	<link href="https://api.fontshare.com/v2/css?f[]=satoshi@1,2&display=swap" rel="stylesheet">
+	<!-- Satoshi Font --> 
+	<link rel="preconnect" href="https://api.fontshare.com/" />
+	<link href="https://api.fontshare.com/v2/css?f[]=satoshi@1,2&display=swap" rel="stylesheet" />
 
-	<!-- Inter font -->
-	<link rel="preconnect" href="https://rsms.me/">
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-	<link rel="stylesheet" href="https://rsms.me/inter/inter.css">
+	<!-- Inter Font -->
+	<link rel="preconnect" href="https://rsms.me/" />
+	<link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
 </svelte:head>
 
+<Header />
+
 {@render children()}
+
+<Footer />
